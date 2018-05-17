049829c4523184656de04d8279ce9e11e03129f2
Imran Sarder <imrancluster@gmail.com>
2018-05-17 15:28:09 +0600

    Added new template to display results

---
+++ templates/mrb-operation-result.tpl.php
@@ -0,0 +1,5 @@
+<div class="mrb-operation-result">
+    <p>Operation started from <strong><?php print $data['start_date'] ?></strong> to <strong><?php print $data['end_date'] ?></strong></p>
+    <p>Total <?php print $data['new'] ?> new MRB created</p>
+    <p>Total <?php print $data['update'] ?> existing MRB updated</p>
+</div>
